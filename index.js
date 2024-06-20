b5a3702e16236495bf6bac610d48a18bdf07627b
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-06-20 10:43:42 +0530

    ADD: functionalities sfo version 1 working;

--- index.js
+++ index.js
@@ -13,34 +13,62 @@ app.use(bodyParser.urlencoded({extended:true}));
 const bot = new Telegraf(envVariables.BOT_ID);
 
 
+bot.start((ctx) => ctx.reply(
+`Hello and welcome to the PAY-ME BOT
+for the payment you want to do, write data in following format--
+amount,customer name, customer email, customer mobile`
+))
+
+
+bot.on('text',async(ctx) => {
+    let user_input = ctx.message.text
+    let parse_input = user_input.split(',').map(item => item.trim());
+
+    await createOrder(parse_input[0],parse_input[1],parse_input[2],parse_input[3], ctx)
+
+})
+
+
 function txn_id () {
     const time = Date.now()
     const randomNumber = Math.floor(Math.random() * 100000);
     return `TXNID-${time}-${randomNumber}`;
 }
 
-const createOrder = async() => {
+const createOrder = async( user_amount, user_name, user_email, user_mobile_no, ctx) => {
     try {
         const response = await axios.post('https://api.ekqr.in/api/create_order', {
             "key": `${envVariables.API_KEY}`,
           "client_txn_id": txn_id(),
-          "amount": "100",
+          "amount": user_amount,
           "p_info": "Product Name",
-          "customer_name": "Jon Doe",
-          "customer_email": "jondoe@gmail.com",
-          "customer_mobile": "9876543210",
+          "customer_name": user_name,
+          "customer_email": user_email,
+          "customer_mobile": user_mobile_no,
           "redirect_url": "http://google.com",
           "udf1": "user defined field 1 (max 25 char)",
           "udf2": "user defined field 2 (max 25 char)",
           "udf3": "user defined field 3 (max 25 char)"
           })
-          console.log (response.data)
+          const qr_url = response.data.data.payment_url
+          await qr(qr_url, ctx);
+
     }catch (error) {
+        ctx.reply('There was an error fetching the QR code. Please try again.');
         console.error('Error creating order:', error);
     }
 }
 
-
+const qr = async(qr_url, ctx) => {
+    try {
+        await ctx.reply(
+    `click the following link to make the payment
+    ${qr_url}
+    for further payments make `);
+    } catch (error) {
+        console.log ("ERR-- error in fetching the qr", error)
+    }
+}
 
 const PORT = 3000;
 app.listen(PORT, async () => {
@@ -48,7 +76,6 @@ app.listen(PORT, async () => {
   const url = await ngrok.connect(PORT);
   console.log(`ngrok tunnel created at: ${url}`);
 
-  createOrder();
-  bot.launch();
+   bot.launch();
 
 });
